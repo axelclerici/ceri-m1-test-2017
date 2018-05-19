b5b2e234b78c3c8e87f3a44ce18324363acb310f
Poisson Blob <Poisson Blob@PoissonBlob-PC>
2018-05-19 18:50:50 +0200

    ajout du test pour get de gamestate provider

--- src/test/java/fr/univavignon/rodeo/impl/GameStateProviderTest.java
+++ src/test/java/fr/univavignon/rodeo/impl/GameStateProviderTest.java
@@ -2,29 +2,19 @@ package fr.univavignon.rodeo.impl;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
+import fr.univavignon.rodeo.api.IAnimal;
+import fr.univavignon.rodeo.api.IEnvironment;
 import fr.univavignon.rodeo.api.IGameState;
 
 public class GameStateProviderTest 
 {
-	/*
-	Map<Specie, Integer> map = new HashMap<Specie, Integer>();
-	List<Animal> caught = new ArrayList<Animal>();
-	caught.add(new Animal("JeanClaudeVanDamme", 600, true, false, false));
-	map.put(new Specie("OUI", 0, null), 33);
-	map.put(new Specie("OUI2", 0, null), 35);
-	map.put(new Specie("OUI3", 0, null), 53);
-	map.put(new Specie("OUI78", 0, null), 55);
-	GameStateProvider provider = new GameStateProvider();
-	EnvironmentProvider envProvider = new EnvironmentProvider();
-	IEnvironment env = envProvider.getEnvironment("Jungle");
-	GameState gameState = new GameState("name", 50, env, 5, caught, map);
-	provider.save(gameState);
-	System.out.println("done");
-	
-*/
-	
 	@Test
 	public void testGetGS()
 	{
@@ -36,5 +26,30 @@ public class GameStateProviderTest
 		assertEquals("Tundra", gs.currentEnvironment.getName());
 		assertEquals(0, gs.allSpecieLevels.get("Emu"));
 		assertEquals("BossTrich", gs.caughtAnimals.get(0).getName());
+	}
+	
+	@Test
+	public void testSave()
+	{
+		GameStateProvider provider = new GameStateProvider();
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		List<IAnimal> caught = new ArrayList<IAnimal>();
+		caught.add(new Animal("JeanClaudeVanDamme", 600, true, false, false));
+		map.put("OUI", 33);
+		map.put("OUI2", 66);
+		map.put("OUI3", 99);
+		EnvironmentProvider envProvider = new EnvironmentProvider();
+		IEnvironment env = envProvider.getEnvironment("Jungle");
+		GameState gameState = new GameState("name", 50, env, 5, caught, map);
+		provider.save(gameState);
+		
+		GameState gs = (GameState) provider.get("name");
+		assertEquals("name", gs.getName());
+		assertEquals(50, gs.getProgression());
+		assertEquals(5, gs.currentArea);
+		assertEquals("Jungle", gs.currentEnvironment.getName());
+		assertEquals(33, gs.allSpecieLevels.get("OUI"));
+		assertEquals("JeanClaudeVanDamme", gs.caughtAnimals.get(0).getName());
+
 	}
 }
